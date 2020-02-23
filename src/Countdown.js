f09a744407856a6bd58e38c3d964aacbdc7ea66a
kendalwon <kendalwon@gmail.com>
2020-02-23 14:08:46 -0500

    reconfigure

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,35 +1,99 @@
 import React from 'react';
 import './Countdown.css';
 
-const Countdown = props => {
+class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shown: true,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      interval: 0
+    }
+    this.cancelCountdown = this.cancelCountdown.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("mounting");
+    let interval = setInterval(() => this.countdown(), 1000);
+    this.setState({interval: interval});
+  }
+
+  countdown() {
+    console.log("counting down");
+    console.log(this.props.eventDate);
+    let d, h, m, s;
+    const currentTime = new Date();
+    let currentTimeInMS = currentTime.getTime();
+    let date = new Date(this.props.eventDate) ;
+    console.log(date);
+    let eventTimeInMS = date.getTime() + 5*60*60*1000;
+    if (this.props.eventTime !== 0) {
+      const a = this.props.eventTime.split(':');
+      const h = a[0] * 60 * 60 * 1000;
+      const m = a[1] * 60 * 1000;
+      const t = h + m;
+      eventTimeInMS += t;
+    }
+    let diff = eventTimeInMS - currentTimeInMS;
+    const daysInMS = 24 * 60 * 60 * 1000;
+    const hoursInMS = 60 * 60 * 1000;
+    const minutesInMS = 60 * 1000;
+    d = Math.floor(diff / daysInMS);
+    h = Math.floor((diff - d*daysInMS)/hoursInMS);
+    m = Math.floor((diff - d*daysInMS - h*hoursInMS)/minutesInMS);
+    s = Math.floor((diff - d*daysInMS - h*hoursInMS - m*minutesInMS)/1000);
+    this.setState({
+      days: d,
+      hours: h,
+      minutes: m,
+      seconds: s
+    });
+  }
+
+  cancelCountdown() {
+    clearInterval(this.state.interval);
+    this.setState({
+      shown: false,
+      interval: 0}
+      );
+  }
+
+  render() {
   return (
-    <div id="countdown-div" className="countdown-div">
-      <h1 id="title" className="title">Countdown to: {props.eventTitle}</h1>
-      <div className="timer">
-        <div className="timer-container">
-          <h1 id="days" className="timer-number">{props.days}</h1>
-          <h2 id="days-label" className="timer-label">days</h2>
-        </div>
-        <div className="timer-container">
-          <h1 id="hours" className="timer-number">{props.hours}</h1>
-          <h2 id="hours-label" className="timer-label">hours</h2>
-        </div>
-        <div className="timer-container">
-          <h1 id="minutes" className="timer-number">{props.minutes}</h1>
-          <h2 id="minutes-label" className="timer-label">minutes</h2>
-        </div>
-        <div className="timer-container">
-          <h1 id="seconds" className="timer-number">{props.seconds}</h1>
-          <h2 id="seconds-label" className="timer-label">seconds</h2>
-        </div>
-      </div>
-      <button 
-        id={props.cancelButton} 
-        className="cancelButton" 
-        type="button" 
-        onClick={props.cancelCountdown}>Cancel</button>
-    </div>
-  )
+    <>
+      {this.state.shown &&
+        <div id="countdown-div" className="countdown-div">
+          <h1 id="title" className="title">Countdown to: {this.props.eventTitle}</h1>
+          <div className="timer">
+            <div className="timer-container">
+              <h1 id="days" className="timer-number">{this.state.days}</h1>
+              <h2 id="days-label" className="timer-label">days</h2>
+            </div>
+            <div className="timer-container">
+              <h1 id="hours" className="timer-number">{this.state.hours}</h1>
+              <h2 id="hours-label" className="timer-label">hours</h2>
+            </div>
+            <div className="timer-container">
+              <h1 id="minutes" className="timer-number">{this.state.minutes}</h1>
+              <h2 id="minutes-label" className="timer-label">minutes</h2>
+            </div>
+            <div className="timer-container">
+              <h1 id="seconds" className="timer-number">{this.state.seconds}</h1>
+              <h2 id="seconds-label" className="timer-label">seconds</h2>
+            </div>
+          </div>
+          <button 
+            id="cancelButton" 
+            className="cancelButton" 
+            type="button" 
+            onClick={this.cancelCountdown}>Cancel</button>
+        </div> 
+      }
+    </>
+  )}
 }
 
 export default Countdown;
--- src/EventForm.js
+++ src/EventForm.js
@@ -1,37 +1,104 @@
 import React from 'react';
 import './EventForm.css';
+import Countdown from './Countdown';
 
-const EventForm = props => {
-  return (
-    <form className="form">
+class EventForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventFormShown: true,
+      events: []
+    }
+    this.addAnotherEvent = this.addAnotherEvent.bind(this);
+  }
+  
+  newEvent(e) {
+    e.preventDefault();
+    let title = e.target.eventTitle.value;
+    let date = e.target.eventDate.value;
+    let time = e.target.eventTime.value;
+    let event = {
+      eventTitle: title,
+      eventDate: date,
+      eventTime: time,
+      countdown: {
+        shown: false,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
+    }
+    this.setState({
+      eventFormShown: false,
+      events: [...this.state.events, event]
+    });
+  }
+  
+  addAnotherEvent() {
+    this.setState({
+      eventFormShown: true
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {
+          (!this.state.eventFormShown) ? 
+          <div className="addAnother">
+            <button 
+            id="addButton" 
+            className="addButton"
+            type="button" 
+            onClick={this.addAnotherEvent}>Add Another Event</button>
+          </div> 
+        : 
+        <form className="form" onSubmit={this.newEvent.bind(this)}>
           <input 
-            id={props.eventTitle} 
+            id="eventTitle"
+            name="eventTitle"
             className="eventTitle" 
             type="text" 
             placeholder="enter your event name"
-            onChange={props.titleChange} 
           />
           <input 
-            id={props.eventDate}  
+            id="eventDate" 
+            name="eventDate" 
             className="eventDate"
             type="date" 
             defaultValue="2020-02-29"
-            onChange={props.dateChange}
           />
           <input 
-            id={props.eventTime} 
-            className="eventTime" 
+            id="eventTime"
+            name="eventTime"           className="eventTime" 
             type="time"
             defaultValue="13:00"
-            onChange={props.timeChange}
           />
-          <button 
-            id={props.startButton} 
+          <button           
+            id="startButton"
             className="startButton" 
-            type="button" 
-            onClick={props.startCountdown}>Start</button>
+            type="submit">
+              Start
+          </button>
         </form>
-  )
+        }
+        {
+          (this.state.events.length > 0) ?
+          this.state.events.map(event => {
+            return (
+            <div key={event.eventTitle}>
+              <Countdown 
+                eventTitle={event.eventTitle}
+                eventDate={event.eventDate}
+                eventTime={event.eventTime}
+              />
+            </div>
+          )}) : null
+        }
+      </>
+    );
+  }
 }
 
 export default EventForm;
